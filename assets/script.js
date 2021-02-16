86ea4e0f9632c3c4a0335f3d75ca8425222c5537
Tuan Nguyen <nvatuan3@gmail.com>
2021-02-16 15:16:05 +0700

    OK

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
     var xhttp = new XMLHttpRequest()
-    xhttp.open("GET", 'http://127.0.0.1:5000//onload', true);
+    xhttp.open("GET", '/onload', true);
     xhttp.send();
 }
