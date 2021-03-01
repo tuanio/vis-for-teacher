771da29e64358eefea89032e1ece2053bbd09f86
TuanNguyenVanAnh <nvatuan3@gmail.com>
2021-03-01 17:10:52 +0700

    Chuyển code từ promise bình thường sang async/await

--- dashboard/assets/script.js
+++ dashboard/assets/script.js
@@ -1,32 +1,3 @@
-let Ajax = function() {
-
-    this.get = (url, callback) => {
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = () => {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                callback(xhttp.responseText);
-            }
-        }
-        xhttp.open('GET', url, true);
-        xhttp.send();
-    }
-
-    this.post = (url, data, callback) => {
-        // data is a json
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = () => {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                callback(xhttp.responseText);
-            }
-        }
-        xhttp.open('POST', url, true);
-        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;UTF-8');
-        xhttp.send(data);
-    }
-}
-
-let ajax = new Ajax();
-
 // this function will run after the page is loaded
 
 window.onload = () => {
@@ -39,9 +10,9 @@ window.onload = () => {
 }
 
 // hàm này dùng để kiểm tra phần tử có selector là tham số, trả về một promise
-const checkElement = async selector => {
+async function checkElement(selector) {
     while (!document.querySelector(selector)) {
-        await new Promise(r => setTimeout(r, 500));
+        await new Promise(r => setTimeout(r, 200));
     }
     return document.querySelector(selector);
-};
+}
